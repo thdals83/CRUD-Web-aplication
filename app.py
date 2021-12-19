fb09c3202d5ece56c9a482505b6a8dfe9aec538f
thdals83 <thdals83@naver.com>
2021-12-19 20:14:00 +0900

    리스트 작성페이지

--- app.py
+++ app.py
@@ -1,26 +1,24 @@
 import hashlib
-
 import jwt
+from bson import ObjectId
 from pymongo import MongoClient
 from datetime import datetime, timedelta
 from flask import Flask, render_template, jsonify, request, redirect, url_for
 
 app = Flask(__name__)
-
 client = MongoClient('localhost', 27017)
 db = client.crud
-
 
 SECRET_KEY = 'SPARTA'
 @app.route('/')
 def home():
-
     token_receive = request.cookies.get('mytoken')
+
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
         member = db.user.find_one({"id": payload['id']})['id']
+
         schd_data = list(db.schedule.find({'id': member}))  #회원정보(member) db와 스케줄정보(schedule) db를 id 값으로 연동
-        area = ["60", "126"]
 
         # 토큰정보(유저의 ID)를 통해 현재 접속자의 스케줄 데이터를 조회
         schd_data = list(db.schedule.find({'id': member}))
@@ -29,7 +27,7 @@ def home():
             # 시간 정보 AM/PM 포맷팅 (hh:mm -> ex) AM 10:30)
             item['time'] = datetime.strftime(datetime.strptime(item['time'], '%H:%M'), '%p %I:%M')
 
-        return render_template('list.html', schd_data=schd_data, area=area, userId=member)
+        return render_template('list.html', schd_data = schd_data, userId = member)
     except jwt.ExpiredSignatureError:
         return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
     except jwt.exceptions.DecodeError:
@@ -45,10 +43,34 @@ def login():
 def register():
     return render_template("register.html")
 
-# 리스트 경로
-@app.route('/list')
-def listregister():
-    return render_template("list.html")
+# 스케줄 작성 경로
+@app.route('/write')
+def write():
+    token_receive = request.cookies.get('mytoken')
+    print(token_receive)
+
+    if token_receive is not None:
+        return render_template('write.html')
+    else:
+        return render_template('write.html', isLogin=False, msg='로그인 후 이용하세요.')
+
+# 회원가입창에서 회원가입 버튼 누르면 값 받아오기
+@app.route('/register/move', methods=['POST'])
+def moverlogin():
+    id = request.form["id"]
+    pwd = request.form["pwdtxt"]
+    pw_hash = hashlib.sha256(pwd.encode('utf-8')).hexdigest()
+
+    doc = {"id":id, "pwd":pw_hash}
+    db.user.insert_one(doc)
+    return render_template('login.html')
+
+# 회원가입 창에서이메일을 db에서 찾아서 클라이언트로 전달
+@app.route('/sign_up/check_dup', methods=['POST'])
+def check_dup():
+    id_receive = request.form['id_give']
+    exists = bool(db.user.find_one({"id": id_receive}))
+    return jsonify({'result': 'success', 'exists': exists})
 
 # 로그인 창에서 로그인 버튼 누르면 값 받아오기
 @app.route('/api/login', methods=['POST'])
@@ -61,7 +83,8 @@ def api_login():
     if result is not None:
         payload = {
             'id': id_receive,
-            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지 => hour 또는 day
+            #'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지 => hour 또는 day
+            'exp': datetime.utcnow() + timedelta(seconds=60 * 5)  # 로그인 5분 유지 => hour 또는 day
         }
 
         # .decode('utf-8')붙이면 서버에서 작동가능, 떼면 로컬에서 작동
@@ -75,25 +98,7 @@ def api_login():
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
 
-# 회원가입창에서 회원가입 버튼 누르면 값 받아오기
-@app.route('/register/move', methods=['POST'])
-def moverlogin():
-    id = request.form["id"]
-    pwd = request.form["pwdtxt"]
-    pw_hash = hashlib.sha256(pwd.encode('utf-8')).hexdigest()
 
-    doc = {"id":id, "pwd":pw_hash}
-    db.user.insert_one(doc)
-    return render_template('login.html')
-
-
-#[아이디 중복확인 API]
-# 이메일을 db에서 찾아서 클라이언트로 전달
-@app.route('/sign_up/check_dup', methods=['POST'])
-def check_dup():
-    id_receive = request.form['id_give']
-    exists = bool(db.user.find_one({"id": id_receive}))
-    return jsonify({'result': 'success', 'exists': exists})
 
 '''
 # API 역할을 하는 부분
